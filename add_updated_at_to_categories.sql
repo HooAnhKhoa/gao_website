5960be1bbf78e6d88513378649e4c354f3f18cc1
Ho Anh Khoa <anhkhoa12204@gmai.com>
2026-02-02 11:31:39 +0700

    done again

--- add_updated_at_to_categories.sql
+++
@@ -1,7 +0,0 @@
--- Thêm cột updated_at vào bảng categories
-ALTER TABLE `categories` 
-ADD COLUMN `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() 
-AFTER `created_at`;
-
--- Cập nhật giá trị updated_at cho các bản ghi hiện có
-UPDATE `categories` SET `updated_at` = `created_at` WHERE `updated_at` IS NULL;
